f8415e43d02c7486bc6e63ad4f0cce02a45129ca
Batmunkh Moltov <m.batmunkh@yahoo.com>
2012-12-22 03:47:29 +0800

    files added

--- web/index.php
+++ web/index.php
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 <?php
     require_once("config.php");
     require_once("../library/bootstrap.php");
     
     
 ?>
-=======
-Home page
->>>>>>> 6f27691271aab36bfe1311add7381dad0663d21d
